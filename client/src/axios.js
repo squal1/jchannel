66af40b9038f314a07b792f810de1b5bf50a2b91
Stephen Chau <chtzch0713@gmail.com>
2022-07-28 15:36:36 -0700

    base URL fix 2

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+const port = process.env.PORT || "8000";
+
 const instance = axios.create({
-    baseURL: process.env.baseURL || "http://localhost:8000",
+    baseURL: process.env.baseURL + port || "http://localhost:8000",
 });
 
 instance.defaults.withCredentials = true;
+
+console.log("PORT:", process.env.baseURL + port);
 
 export default instance;
