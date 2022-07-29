d4d0c168ade35c1b5ccbc21cef7173997176ecdb
Stephen Chau <chtzch0713@gmail.com>
2022-07-28 17:20:21 -0700

    cors fix 3

--- server.js
+++ server.js
@@ -40,15 +40,11 @@ app.use(
         origin: [
             "http://localhost:3000",
             "http://localhost:8000",
-            "https://jchannel.herokuapp.com",
+            "https://jchannel.herokuapp.com" + ":" + port,
         ],
         credentials: true,
     })
 );
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
 
 //app.use(express.static(path.join(__dirname + "/client/build")));
 if (process.env.NODE_ENV === "production") {
