255f47ed0c066c9065b4d166efa487f884894176
Stephen Chau <chtzch0713@gmail.com>
2022-07-28 17:13:32 -0700

    cors fix 2

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use(
     })
 );
 app.use(function (req, res, next) {
-    res.header("Access-Control-ALlow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
 //app.use(express.static(path.join(__dirname + "/client/build")));
