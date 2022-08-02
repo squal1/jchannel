0fe771f8b0f9aaf7e09e1cd00aa7a5b90dd63438
Stephen Chau <chtzch0713@gmail.com>
2022-08-01 17:29:30 -0700

    nav bar css fix

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(
     })
 );
 
-app.use(express.static(path.join(__dirname + "/client/build")));
+//app.use(express.static(path.join(__dirname + "/client/build")));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname + "/client/build")));
 }
